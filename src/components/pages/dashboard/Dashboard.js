bb05d15c17ced1ba8e929f87190357c3b9e7ea97
Paul Bartocillo <paulbartocillo@gmail.com>
2020-07-20 16:45:13 +0800

    Resize

--- src/components/pages/dashboard/Dashboard.js
+++ src/components/pages/dashboard/Dashboard.js
@@ -25,10 +25,11 @@ const Dashboard = () => {
     <>
       <Typography.Title level={3} style={styles.titleStyle}>
         Photo Uploader
+        <ButtonComponent type='primary' onClick={capture} style={{ paddingBottom: '40px' }}>
+          Capture Photo
+        </ButtonComponent>
       </Typography.Title>
-      <ButtonComponent type='primary' onClick={capture} style={{ paddingBottom: '40px' }}>
-        Capture Photo
-      </ButtonComponent>
+
       <Text type='secondary'>Any fool can write code that a computer can understand. Good programmers write code that humans can understand.</Text>
       <Webcam videoConstraints={videoConstraints} width={280} audio={false} ref={webcamRef} screenshotFormat='image/jpeg' />
       {imgSrc && <img alt='' src={imgSrc} width={280} style={{ paddingTop: '60px', background: 'transparent' }} />}
