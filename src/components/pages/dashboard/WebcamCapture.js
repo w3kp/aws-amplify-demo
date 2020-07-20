4f108ae4525d59ecbb4a141f6eee9ca7c13016a5
Paul Bartocillo <paulbartocillo@gmail.com>
2020-07-20 16:15:03 +0800

    Resize

--- src/components/pages/dashboard/WebcamCapture.js
+++ src/components/pages/dashboard/WebcamCapture.js
@@ -6,8 +6,8 @@ const WebcamCapture = () => {
   const videoConstraints = {
     width: { min: 1024 },
     height: { min: 1024 },
-    // facingMode: { exact: 'environment' },
-    facingMode: 'user',
+    facingMode: { exact: 'environment' },
+    // facingMode: 'user',
   }
 
   const webcamRef = React.useRef(null)
@@ -20,10 +20,10 @@ const WebcamCapture = () => {
 
   return (
     <>
-      <Webcam videoConstraints={videoConstraints} width={280} audio={false} ref={webcamRef} screenshotFormat='image/jpeg' />
       <ButtonComponent type='primary' onClick={capture} style={{ paddingBottom: '40px' }}>
         Capture Photo
       </ButtonComponent>
+      <Webcam videoConstraints={videoConstraints} width={280} audio={false} ref={webcamRef} screenshotFormat='image/jpeg' />
       {imgSrc && <img alt='' src={imgSrc} width={280} style={{ paddingTop: '60px', background: 'transparent' }} />}
     </>
   )
